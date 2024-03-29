3e38c83f3d2df020fbc1f35a4a61af9ceebb7dfa
YengSokroza <yengsokroza@gmail.com>
2024-03-29 15:19:01 +0700

    new thumbnail picture

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -13,7 +13,7 @@ export const metadata : Metadata = {
 	openGraph:{
 		title: "ISTAD Ecommerce Web",
 		description: "ISTAD Ecommerce Web is a web application for selling products",
-		images: 'https://i.pinimg.com/564x/2c/d2/bb/2cd2bb67520620ed94d2734bc407489a.jpg'
+		images: 'https://store.istad.co/media/brand_images/thumbnailstore.jpg'
 	}
 }
 export default function RootLayout({
