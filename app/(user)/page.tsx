69dce34a578b72ec0a70b14ea4dc8b8865656967
YengSokroza <yengsokroza@gmail.com>
2024-03-30 17:33:00 +0700

    fix user page

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -37,13 +37,17 @@ export default function Home() {
 
 	return (
 
-		<main >
-			<ContainerScroll titleComponent={<HeroText />} children={<VideoComponent />} />
+		<main className="xl:pt-20 pt-8">
+			<div className="pt-20">
+				<ContainerScroll titleComponent={<HeroText />}>{<VideoComponent />}</ContainerScroll>
+			</div>
+			
+			
 			<section className=" bg-red-60 " >
-				<div className=" p-16 h-screen rounded-lg xl:space-y-4">  
+				<div className=" p-16  rounded-lg space-y-4 ">  
 					<p className="xl:text-lg  text-md font-black">Unleash Your Inner You </p>
-					<h1 className="xl:text-6xl  text-3xl font-semibold">Discover's Big Style Sale!</h1>
-					<p className="xl:text-lg  text-md font-medium">Get ready to express yourself with incredible deals on clothes, jewelry, and bags for both men and women! Discover your perfect outfit at a fraction of the price. Don't miss out – this sale won't last forever!</p>
+					<h1 className="xl:text-6xl  text-3xl font-semibold">Discover&apos;s Big Style Sale!</h1>
+					<p className="xl:text-lg  text-md font-medium">Get ready to express yourself with incredible deals on clothes, jewelry, and bags for both men and women! Discover your perfect outfit at a fraction of the price. Don&apos;t miss out  this sale won&apos;t last forever!</p>
 					<button className="bg-gray-50 px-8 py-2 rounded-md font-semibold text-gray-800" onClick={() => {
 						router.push('./service')
 					}} >Shop Now </button>
@@ -51,7 +55,7 @@ export default function Home() {
 				</div>
 				<div className=" grid xl:grid-cols-4 grid-cols-1 gap-4">
 					<div className="bg-white">
-							
+
 					</div>
 					<div></div>
 					<div></div>
--- components/card/SliderComponent.tsx
+++ components/card/SliderComponent.tsx
@@ -10,15 +10,15 @@ import thumbnail4 from "@/public/thumbnail4.svg"
 
 export default function SliderComponent() {
     return (
-        <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+        <div className="hidden sm:block sm:h-64 xl:h-80 2xl:h-96">
             <Carousel onSlideChange={(index) => console.log('onSlideChange()', index)}>
-                <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
+                <div className="flex h-full items-center justify-center bg-transparent dark:bg-gray-700 dark:text-white">
                     <Image src={thumbnail3} alt="thumbnail" layout="responsive" objectPosition="center" />
                 </div>
-                <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
+                <div className="flex h-full items-center justify-center bg-transparent dark:bg-gray-700 dark:text-white">
                     <Image src={thumbnail2} alt="thumbnail" layout="responsive" objectPosition="center" />
                 </div>
-                <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
+                <div className="flex h-full items-center justify-center bg-transparent dark:bg-gray-700 dark:text-white">
                     <Image src={thumbnail4} alt="thumbnail" layout="responsive" objectPosition="center" />
                 </div>
             </Carousel>
